3156663a3334f4d40b87c5d04f8376882cb8f0e6
mlubega <3893519+mlubega@users.noreply.github.com>
2019-08-17 18:24:28 +0100

    draws three shapes

--- maskfromjson.py
+++ maskfromjson.py
@@ -20,7 +20,7 @@ from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
 
 data_file = "/home/aries/ds_course/term3/bee_export.json"
-mask_dest_file = "/home/aries/ds_course/term3/bee_data/masks_sm/"
+mask_dest_file = "/home/aries/ds_course/term3/bee_data/masks_20_binary/"
 IMG_DIM = 1025
 
 
@@ -59,7 +59,7 @@ def main():
                 logger.debug("Processsing %s", filename)
                 
                 #create black image on which to draw regions
-                mask = np.zeros((IMG_DIM,IMG_DIM,3), np.uint8)
+                mask = np.zeros((IMG_DIM,IMG_DIM), np.uint8)
                 
                 # iteratively add shapes
                 regions = img["regions"]
@@ -68,13 +68,12 @@ def main():
                 for region in regions:
                     shape = region["shape_attributes"]["name"]
                     bee_type = region["region_attributes"]["class"]
-                    color = CLASS_COLORS[bee_type]
-                    
-                    logger.debug("Shape: %s, Class: %s", shape, bee_type)
-                    
+                    #color = CLASS_COLORS[bee_type]
+                    color = 255
                     
                     #drawing functions
                     if shape == "circle":
+                        
                         x = int(region["shape_attributes"]["cx"])
                         y = int(region["shape_attributes"]["cy"])
                         radius = int(region["shape_attributes"]["r"])
@@ -88,26 +87,26 @@ def main():
                         ry = int(region["shape_attributes"]["ry"])
                         theta = region["shape_attributes"]["theta"]
                         angle = np.degrees(theta)
-                        logger.debug("Radians to Deg: %f to %f ----- args: %i, %i, %i, %i, %f, %s", theta, angle,x,y,rx,rx,angle,color)
                     
-                        cv2.ellipse(mask, (x,y), (rx, ry), angle, 0, 360, color, -1 , shift = 3)
+                        cv2.ellipse(mask, (x,y), (rx, ry), angle, 0, 360, color, -1)
                         
                     elif shape == "polygon":
-                        break;
+                        
                         vertices = zip(region["shape_attributes"]["all_points_x"],
                                        region["shape_attributes"]["all_points_y"])
                         
+                        
                         # change from list of tuples to list of array
                         vertices = np.array(list(map(list, vertices)), np.int32)
-                        vertices = vertices.reshape((-1, 1, 2))
-                        cv2.polylines(mask, [vertices], True, color, -1)
-                        
+    
+                        cv2.fillPoly(mask, [vertices], color)
                     else:
                         print(shape)
                         raise TypeError(shape, "is not supported")
                     
                 plt.imshow(mask)
                 plt.show()
+                cv2.imwrite(os.path.join(mask_dest_file, filename), mask)
                         
                 
                 
