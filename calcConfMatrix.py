d2afc984ea4716bdbc9233310161058508c0569a
mlubega <3893519+mlubega@users.noreply.github.com>
2019-08-30 20:22:52 +0100

    basic flow in place

---
+++ calcConfMatrix.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 18:11:07 2019
+
+@author: aries
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import os
+import logging
+import json
+import cv2 
+import argparse 
+import glob
+
+import numpy as np
+
+
+
+class DataFormatError(Exception):
+    """Raised when data not in correct format"""
+    pass
+
+gt_path = "/home/aries/ds_course/term3/bee_data_1024/gt_class_masks_1024"
+unet_path = "/home/aries/ds_course/term3/bee_data_1024/unet_predictions_class_mask_1024"
+
+classes = {'Bee':0, 'Abdomen': 1, 'Background': 2}
+
+addition = { 70:['Bee', 'Bee'],
+             200: ['Abdomen', 'Abdomen'],
+             0: ['Background', 'Background']}
+
+subtraction = {-65: ['Bee', 'Abdomen'],
+               35: ['Bee', 'Background'],
+               65: ['Abdomen', 'Bee'],
+               100: ['Abdomen', 'Bee'],
+               -35: ['Background', 'Bee'],
+               -100: ['Background', 'Abdomen']}
+
+NUM_CLASSES = 3
+
+conf_matrix = np.zeros((NUM_CLASSES, NUM_CLASSES)) 
+
+gt_paths = glob.glob(os.path.join(gt_path , "*.png"))
+unet_paths = glob.glob(os.path.join(unet_path, "*.png"))
+
+assert len(gt_paths) == len(unet_paths)
+    
+gt_paths.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
+unet_paths.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
+
+paired_masks = list(zip(gt_paths, unet_paths))
+
+for gt_path, unet_path in paired_masks:
+    if not os.path.basename(gt_path) == os.path.basename(unet_path):
+        raise DataFormatError("Names of ground truth and predicted masks must match")
+        
+
+    gt_img = cv2.imread(gt_path) #, cv2.IMREAD_GRAYSCALE)
+    unet_img = cv2.imread(unet_path) #, cv2.IMREAD_GRAYSCALE)
+
+
+    gt = gt_img.astype(int)
+    unet = unet_img.astype(int)
+    
+    added = gt + unet
+    
+    subtracted = gt - unet
+    #print(np.unique(gt_img))
+    #print(np.unique(unet_img))
+    print(np.unique(added, return_counts=True))
+    print(np.unique(subtracted, return_counts=True))
+    
+    
+    break
+
+    #plt.imshow(gt_img)
+    #plt.show
+    
+    #plt.imshow(unet_img)
+    #plt.show
+                 
+    #cv2.imshow('GT',gt_img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+                 
+    #cv2.imshow('UNET',unet_img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+
+
+#plt.imshow(unet_img)
+#plt.show
+
+
+
